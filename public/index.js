8c5766317b9ccc6b9f3b563235781e8474e5f166
hahahah67-pixel <olivercartelli676767@gmail.com>
2026-02-24 20:16:42 -0800

    index.js

--- public/index.js
+++ public/index.js
@@ -1,24 +1,11 @@
 "use strict";
-/**
- * @type {HTMLFormElement}
- */
+
 const form = document.getElementById("uv-form");
-/**
- * @type {HTMLInputElement}
- */
 const address = document.getElementById("uv-address");
-/**
- * @type {HTMLInputElement}
- */
 const searchEngine = document.getElementById("uv-search-engine");
-/**
- * @type {HTMLParagraphElement}
- */
 const error = document.getElementById("uv-error");
-/**
- * @type {HTMLPreElement}
- */
 const errorCode = document.getElementById("uv-error-code");
+
 const connection = new BareMux.BareMuxConnection("/baremux/worker.js");
 
 form.addEventListener("submit", async (event) => {
@@ -35,16 +22,23 @@ form.addEventListener("submit", async (event) => {
 	const url = search(address.value, searchEngine.value);
 
 	let frame = document.getElementById("uv-frame");
+
+	// 🔥 Hide homepage completely
+	document.getElementById("home-ui").style.display = "none";
+
 	frame.style.display = "block";
+
 	let wispUrl =
 		(location.protocol === "https:" ? "wss" : "ws") +
 		"://" +
 		location.host +
 		"/wisp/";
+
 	if ((await connection.getTransport()) !== "/epoxy/index.mjs") {
 		await connection.setTransport("/epoxy/index.mjs", [
 			{ wisp: wispUrl },
 		]);
 	}
+
 	frame.src = __uv$config.prefix + __uv$config.encodeUrl(url);
 });

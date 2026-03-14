c79924a9c110b12d6dca745e2b811ad2a268677c
hahahah67-pixel <olivercartelli676767@gmail.com>
2026-03-14 13:07:35 -0700

    config.js

--- public/config.js
+++ public/config.js
@@ -1,1 +1,12 @@
-let _CONFIG = {};
+// This file overwrites the stock UV config.js
+
+self.__uv$config = {
+	prefix: "/uv/service/",
+	encodeUrl: Ultraviolet.codec.xor.encode,
+	decodeUrl: Ultraviolet.codec.xor.decode,
+	handler: "/uv/uv.handler.js",
+	client: "/uv/uv.client.js",
+	bundle: "/uv/uv.bundle.js",
+	config: "/uv/uv.config.js",
+	sw: "/uv/uv.sw.js",
+};

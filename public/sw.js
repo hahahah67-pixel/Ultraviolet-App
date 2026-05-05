4a722e69985ac6a97338894fb7f8265ebe8e9a01
hahahah67-pixel <olivercartelli676767@gmail.com>
2026-05-05 13:21:01 -0700

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-importScripts("/scram/scramjet.js");
+importScripts("/scram/scramjet.all.js");
 
 const { ScramjetServiceWorker } = $scramjetLoadWorker();
 const scramjet = new ScramjetServiceWorker();

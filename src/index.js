2337672f703b30ee358809ec2c855f72da90ba66
hahahah67-pixel <hahahah67-pixel@github.com>
2026-06-03 03:44:01 +0000

    Fix Scramjet engine: serve from public/scram instead of npm dist
    - Removed scramjet npm package dependency (not needed)
    - Serve /scram/ route from ./public/scram/ where the actual controller and WASM files are
    - Client expects controller.api.js, controller.inject.js, scramjet.js, and scramjet.wasm in /scram/
    - npm dist has different bundled files that don't match client expectations

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import cookieParser from "cookie-parser";
 import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
 import { epoxyPath } from "@mercuryworkshop/epoxy-transport";
 import { baremuxPath } from "@mercuryworkshop/bare-mux/node";
-import { scramjetPath } from "@mercuryworkshop/scramjet/path";
 
 const app = express();
 app.use(cookieParser());
@@ -83,7 +82,7 @@ app.use(express.static("./public"));
 app.use("/uv/", express.static(uvPath));
 app.use("/epoxy/", express.static(epoxyPath));
 app.use("/baremux/", express.static(baremuxPath));
-app.use("/scram/", express.static(scramjetPath));
+app.use("/scram/", express.static("./public/scram"));
 
 function sendError(res, code, file) {
   res.status(code);
